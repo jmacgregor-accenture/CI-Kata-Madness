1f942ffb22b49c11e5ae64c5fa74fffa3743a096
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-10 15:10:20 -0400

    Added backend template

---
+++ DotNet/CIKataBackEnd/CIKataBackEnd.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CIKataBackEnd.WebApi.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIKataBackEnd.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private BusinessLogicService _bls = new BusinessLogicService();
+        
+        // GET api/values/
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var returnString = string.Empty;
+
+            try
+            {
+                returnString = _bls.MakeUpValue();
+            }
+            catch (Exception e)
+            {
+                returnString = $"This Is Not a Successful Call Because: {e.Message}";
+            }
+            
+            return new JsonResult(returnString);
+        }
+    }
+}
